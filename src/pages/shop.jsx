1ae1352a7957fb9d91aba75bd0a241d94095f2c0
Adesoji <sojman007@yahoo.com>
2025-07-22 12:28:33 +0000

    modify shop component to use mock db

--- src/pages/shop.jsx
+++ src/pages/shop.jsx
@@ -1,61 +1,23 @@
-
+import { getAllProducts } from '../data/mock_db';
+import { useState } from 'react';
 
 function Shop() {
+ 
+  const [products] = useState(getAllProducts());
+
   return (
     <div className="shop-container">
       <h1 className="shop-title">Welcome to the Shop</h1>
 
       <div className="product-list-shop">
-
-        <div className="product-card-shop">
-          <img src="public/Samsung-Galaxy-S25-Ultra.webp" className="product-image"/>
-          <h2 className="product-name-shop">Samsung S25ultra</h2>
-          <p className="product-price-shop">&#8358;2,000,000</p>
-          <button className="buy-button-shop">Buy Now</button>
-        </div>
-
-        <div className="product-card-shop">
-          <img src="public/macpro.jpg" className="product-image"/>
-          <h2 className="product-name-shop">Laptop Pro 15</h2>
-          <p className="product-price-shop">&#8358;4,000,000</p>
-          <button className="buy-button-shop">Buy Now</button>
-        </div>
-
-        <div className="product-card-shop">
-          <img src="public/airpodpromax.png" className="product-image"/>
-          <h2 className="product-name-shop">Airpod Max</h2>
-          <p className="product-price-shop">&#8358;800,000</p>
-          <button className="buy-button-shop">Buy Now</button>
-        </div>
-
-
-        <div className="product-card-shop">
-          <img src="public/macpro.jpg" className="product-image"/>
-          <h2 className="product-name">Laptop Pro 15</h2>
-          <p className="product-price-shop">&#8358;4,000,000</p>
-          <button className="buy-button-shop">Buy Now</button>
-        </div>
-
-
-
-
-        <div className="product-card-shop">
-          <img src="public/airpodpromax.png" className="product-image"/>
-          <h2 className="product-name-shop">Airpod Max</h2>
-          <p className="product-price-shop">&#8358;800,000</p>
-          <button className="buy-button-shop">Buy Now</button>
-        </div>
-
-
-       < div className="product-card-shop">
-          <img src="public/Samsung-Galaxy-S25-Ultra.webp" className="product-image"/>
-          <h2 className="product-name-shop">Samsung S25ultra</h2>
-          <p className="product-price-shop">&#8358;2,000,000</p>
-          <button className="buy-button-shop">Buy Now</button>
-        </div>
-
-      
-      
+        {products.map(product => (
+          <div key={product.productId} className="product-card-shop">
+            <img src={product.productImage} alt={product.productName} className="product-image" />
+            <h2 className="product-name-shop">{product.productName}</h2>
+            <p className="product-price-shop">{product.productPrice}</p>
+            <button className="buy-button-shop">Buy Now</button>
+          </div>
+        ))}
       </div>
     </div>
   );
